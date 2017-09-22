6211c72864eb4eefaca32c20d72690faa2b1dd51
Anders Reiche <andersreiche@gmail.com>
2017-09-22 23:59:01 +0200

    commented and cleaned_v2

--- src/Options.h
+++ src/Options.h
@@ -4,15 +4,15 @@
 using namespace std;
 class Options
 {
-protected:
+protected: 				//protected vars are inherrited as private
 	int argC,count;
 	char **argV;
-	string optstring;
+	string optstring;	//optstring is to be tested against in getopt()
 public:
     Options();
     virtual ~Options();
 	void setOptstring(string validopt);
-	virtual string getopt(void) = 0;
+	virtual string getopt(void) = 0;	//=0 makes the class abstract
 	int numopt(void);
 };
 
